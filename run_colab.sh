ff2e6466bbb46d5225d96788994185e362e453e8
sashankmodali <sashankmodali@gmail.com>
2022-07-03 01:20:05 -0400

    fixing returns

--- run_colab.sh
+++ run_colab.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python main.py --algo GenPBE --summary_dir simulation_results --n_episodes 10000
-python main.py --algo saddle_point --summary_dir simulation_results --n_episodes 10000
-python main.py --algo RPBCAC --summary_dir simulation_results --n_episodes 10000
+python main.py --algo GenPBE --summary_dir simulation_results --n_episodes 10000 --fast_lr 0.02 --slow_lr 0.01
+python main.py --algo saddle_point --summary_dir simulation_results --n_episodes 10000 --fast_lr 0.02 --slow_lr 0.01
+python main.py --algo RPBCAC --summary_dir simulation_results --n_episodes 10000 --fast_lr 0.02 --slow_lr 0.01
